db46c847a0a835176e626dd084a2cdcb5ed9b6c7
almalmalm <almostkalinin@gmail.com>
2022-03-12 00:42:18 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,35 @@
+#comment VARIABLES
+#Types
+_cars = 23
+cars = 24
+CARS = 25
+CARS = 28983
+
+number_of_cars = 23
+kind_of_cars = "Ferrari"
+
+#Print function
+print(cars)
+print(_cars)
+print(CARS)
+print(number_of_cars)
+print(kind_of_cars)
+
+"""
+This is a multi-line comment
+python is easy to start
+"""
+
+'''
+and this is too.
+'''
+name = "Leo Kalinin"
+type_of_car = "bmw"
+school = "TAU"
+
+print(name + school)
+print(name + " " + school)
+print(name + " learn at school " + school + ".")
+
+#python string.format()
+print("{} wants to be a SDET, and buy a {}.".format(name,type_of_car))
